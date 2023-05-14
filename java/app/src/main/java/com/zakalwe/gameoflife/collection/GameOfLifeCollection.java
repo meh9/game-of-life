9223ac832fd0c86c48b4a3786a5b02967082ff2c
Mats Henrikson <mats@zakalwe.com>
2023-05-15 09:25:01 +1000

    Close to working collection based algo, only progress() left to implement.

--- java/app/src/main/java/com/zakalwe/gameoflife/collection/GameOfLifeCollection.java
+++ java/app/src/main/java/com/zakalwe/gameoflife/collection/GameOfLifeCollection.java
@@ -13,8 +13,12 @@ import com.zakalwe.gameoflife.GameOfLife;
  */
 public class GameOfLifeCollection implements GameOfLife {
 
-    private final int generation = 0;
-    private final Map<RowCol, Boolean> a = new TreeMap<>();
+    private int generation = 0;
+    private Map<RowCol, Boolean> a = new TreeMap<>();
+    private int minRow = Integer.MAX_VALUE;
+    private int maxRow = Integer.MIN_VALUE;
+    private int minCol = Integer.MAX_VALUE;
+    private int maxCol = Integer.MIN_VALUE;
 
     /** Default constructor */
     public GameOfLifeCollection() {
@@ -40,6 +44,12 @@ public class GameOfLifeCollection implements GameOfLife {
          * continue; // just being explicit
          * }
          */
+        a = new TreeMap<>();
+        minRow = Integer.MAX_VALUE;
+        maxRow = Integer.MIN_VALUE;
+        minCol = Integer.MAX_VALUE;
+        maxCol = Integer.MIN_VALUE;
+        generation++;
     }
 
     /**
@@ -51,7 +61,26 @@ public class GameOfLifeCollection implements GameOfLife {
      */
     @Override
     public void setCell(final int row, final int col, final boolean alive) {
-        a.put(new RowCol(row, col), alive);
+        setCell(new RowCol(row, col), alive);
+    }
+
+    private void setCell(final RowCol rowCol, final boolean alive) {
+        if (alive) {
+            // add the actual live cell
+            a.put(rowCol, alive);
+            // add all the neighbouring dead cells if the do not exist already
+            for (final RowCol rc : rowCol.computeNeighbours(rowCol.row, rowCol.col)) {
+                setCell(rc, false);
+            }
+        } else {
+            // only add dead cells if there is no cell at that coord already, dead or alive
+            a.putIfAbsent(rowCol, false);
+        }
+        // make sure we track min/max bounds
+        minRow = rowCol.row < minRow ? rowCol.row : minRow;
+        maxRow = rowCol.row > maxRow ? rowCol.row : maxRow;
+        minCol = rowCol.col < minCol ? rowCol.col : minCol;
+        maxCol = rowCol.col > maxCol ? rowCol.col : maxCol;
     }
 
     /**
@@ -73,9 +102,12 @@ public class GameOfLifeCollection implements GameOfLife {
      */
     @Override
     public int countLiveCells() {
-        final int count = -1;
-
-        // TODO: implement
+        int count = 0;
+        for (final boolean live : a.values()) {
+            if (live) {
+                count++;
+            }
+        }
         return count;
     }
 
@@ -94,13 +126,19 @@ public class GameOfLifeCollection implements GameOfLife {
         final StringBuilder sb = new StringBuilder();
         sb.append("Generation: ").append(generation);
         sb.append(", Live cells: ").append(countLiveCells());
-        sb.append(", All cells: ").append(countAllCells()); // .append("\n");
-        // for (int row = 0; row < a.length; row++) {
-        // for (int col = 0; col < a[row].length; col++) {
-        // sb.append(a[row][col] ? "■ " : "□ ");
-        // }
-        // sb.append("\n");
-        // }
+        sb.append(", All cells: ").append(countAllCells());
+        sb.append(", Min/Max bounds: ").append((maxRow - minRow) * (maxCol - minCol)).append("\n");
+        for (int row = minRow; row <= maxRow; row++) {
+            for (int col = minCol; col <= maxCol; col++) {
+                final Boolean b = a.get(new RowCol(row, col));
+                if (b == null) {
+                    sb.append("  ");
+                } else {
+                    sb.append(b ? "■ " : "□ ");
+                }
+            }
+            sb.append("\n");
+        }
         return sb.toString();
     }
 
@@ -118,6 +156,23 @@ public class GameOfLifeCollection implements GameOfLife {
                 return col - o.col;
             }
             return rowDiff;
+        }
+
+        private RowCol[] computeNeighbours(final int row, final int col) {
+            final RowCol[] r = new RowCol[8];
+            final int top = row - 1;
+            final int bottom = row + 1;
+            final int left = col - 1;
+            final int right = col + 1;
+            r[0] = new RowCol(top, left);
+            r[1] = new RowCol(top, col);
+            r[2] = new RowCol(top, right);
+            r[3] = new RowCol(row, right);
+            r[4] = new RowCol(bottom, right);
+            r[5] = new RowCol(bottom, col);
+            r[6] = new RowCol(bottom, left);
+            r[7] = new RowCol(row, left);
+            return r;
         }
     }
 }
