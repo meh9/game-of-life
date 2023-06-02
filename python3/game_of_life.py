98f3a534278385091146f3a423f53c115bab9ef3
Mats Henrikson <mats@zakalwe.com>
2023-06-02 17:32:01 +1000

    Start adding ability to load Run Length Encoded files.

--- python3/game_of_life.py
+++ python3/game_of_life.py
@@ -1,11 +1,24 @@
 """Simple Conway's Game of Life implementation."""
 
+import argparse
 from gameoflife import MainGame
 
 
 def main() -> None:
     """Run the game."""
-    MainGame().main()
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        description="Conway's Game of Life"
+    )
+    parser.add_argument(
+        "-r",
+        "--rle",
+        nargs="?",
+        type=argparse.FileType("r"),
+        help="specify a Run Length Encoded file to read initial cells from",
+    )
+    args: argparse.Namespace = parser.parse_args()
+
+    MainGame(rle_file=args.rle).main()
 
 
 if __name__ == "__main__":
