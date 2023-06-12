5c097d442832bbfdeb7a0396b93d7fd1923cfb7e
Mats Henrikson <mats@zakalwe.com>
2023-06-12 18:18:43 +1000

    Parse header and rule.

--- python3/gameoflife/runlengthencoded.py
+++ python3/gameoflife/runlengthencoded.py
@@ -21,23 +21,42 @@ class RunLengthEncoded(FLContextManager):
     """Implements loading Run Length Encoded data from files."""
 
     METADATA_LINE: pp.ParserElement = (
-        (
-            pp.AtLineStart("#").suppress()
-            + pp.one_of("C c N O P R r")
-            + pp.Word(pp.printables + " ")
-            + pp.Suppress(pp.line_end)
-        )
-        .set_results_name("metadata", True)
-        .set_name("metadata")
+        pp.AtLineStart("#").suppress()
+        + pp.one_of("C c N O P R r")
+        + pp.Word(pp.printables + " ")
+        + pp.Suppress(pp.line_end)
+    ).set_results_name("metadata", True)
+
+    int_number: pp.ParserElement = pp.Word(pp.nums).set_parse_action(  # type:ignore
+        lambda t: [int(t[0])]  # type:ignore
     )
 
-    PARSER: pp.ParserElement = pp.ZeroOrMore(METADATA_LINE)
+    HEADER: pp.ParserElement = pp.OneOrMore(
+        (
+            pp.one_of("x y")
+            + pp.Suppress("=")
+            + int_number
+            + pp.Optional(",").suppress()
+        ).set_results_name("header", True)
+    )
+
+    RULE: pp.ParserElement = pp.ZeroOrMore(
+        pp.Keyword("rule")
+        + pp.Suppress("=")
+        + pp.Word(pp.printables + " ")
+        + pp.Suppress(pp.line_end)
+    ).set_results_name("rule")
+
+    PARSER: pp.ParserElement = pp.ZeroOrMore(METADATA_LINE) + HEADER + RULE
 
     def __init__(self, file: str) -> None:
         """Initialise the loader."""
         self._filename: str = file
         self._file: TextIOWrapper
         self.metadata: pp.ParseResults
+        self.cols: int
+        self.rows: int
+        self.rule: str
 
     def cells(self) -> list[list[bool]]:
         """Return an array of cells lopaded from the file."""
@@ -48,6 +67,10 @@ class RunLengthEncoded(FLContextManager):
         self._file = open(self._filename, "r", encoding="UTF-8")
         results: pp.ParseResults = RunLengthEncoded.PARSER.parse_file(self._file)
         self.metadata = results.metadata  # type: ignore
+        self.cols = results.header[0][1]  # type: ignore
+        self.rows = results.header[1][1]  # type: ignore
+        if results.rule:  # type: ignore
+            self.rule = results.rule[1]  # type: ignore
         return self
 
     def __exit__(
@@ -58,3 +81,11 @@ class RunLengthEncoded(FLContextManager):
     ) -> None:
         """Exit context manager."""
         self._file.close()
+
+    def __str__(self) -> str:
+        """To string."""
+        return (
+            f"{self.metadata}\n"
+            + f"cols: {self.cols}, rows: {self.rows}, "
+            + f"rule: {self.rule if hasattr(self, 'rule') else 'none'}"
+        )
--- python3/tests/test_file_loaders.py
+++ python3/tests/test_file_loaders.py
@@ -14,6 +14,9 @@ x = 3, y = 3, rule = B3/S23
 bob$2bo$3o!
 """
 
+HEADER_RULE: str = "x = 4, y = 5, rule = B3/S23"
+HEADER_NO_RULE: str = "x = 4, y = 5"
+
 
 def test_create_loader() -> None:
     """Test the create_loader function."""
@@ -27,8 +30,20 @@ def test_create_loader() -> None:
 def test_run_length_encoded() -> None:
     """Test the RunLengthEncoded file type."""
     with create_loader("../data/glider.rle") as loader:
+        assert loader.__class__ is RunLengthEncoded
+        assert loader.metadata  # type: ignore
+        assert loader.cols  # type: ignore
+        assert loader.rows  # type: ignore
+        assert loader.rule  # type: ignore
         # the below will change when we actually have data
         assert loader.cells()[0][0] is False
+        print(loader)
+        assert (
+            str(loader)
+            == "[['N', 'Glider'], ['O', 'Richard K. Guy'], ['C', 'The smallest, most common, and "
+            + "first discovered spaceship. Diagonal, has period 4 and speed c/4.'], ['C', "
+            + "'www.conwaylife.com/wiki/index.php?title=Glider']]\ncols: 3, rows: 3, rule: B3/S23"
+        )
 
 
 class TestRunLengthEncoded:
@@ -44,3 +59,19 @@ class TestRunLengthEncoded:
         assert results[1] == "Glider"
         assert results[-1] == "www.conwaylife.com/wiki/index.php?title=Glider"
         assert results.metadata[1][1] == "Richard K. Guy"  # type: ignore
+
+    def test_header_rule_parser(self) -> None:
+        """Test the HEADER parser."""
+        results: pp.ParseResults = (
+            RunLengthEncoded.HEADER + RunLengthEncoded.RULE
+        ).parse_string(HEADER_RULE)
+        assert results.header[0][1] == 4  # type:ignore
+        assert results.header[1][1] == 5  # type:ignore
+        assert results.rule[1] == "B3/S23"  # type:ignore
+
+        results = (RunLengthEncoded.HEADER + RunLengthEncoded.RULE).parse_string(
+            HEADER_NO_RULE
+        )
+        assert results.header[0][1] == 4  # type:ignore
+        assert results.header[1][1] == 5  # type:ignore
+        assert len(results.rule) == 0  # type:ignore
