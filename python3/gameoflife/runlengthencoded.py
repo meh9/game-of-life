bd2fa25a5da9120358382551d55c69f12bb2caf1
Mats Henrikson <mats@zakalwe.com>
2023-06-12 15:31:41 +1000

    Start playing with pyparsing for parsing files.

--- python3/gameoflife/runlengthencoded.py
+++ python3/gameoflife/runlengthencoded.py
@@ -3,6 +3,27 @@
 from io import TextIOWrapper
 from types import TracebackType
 from gameoflife import FileLoader, FLContextManager
+import pyparsing as pp
+
+
+# pylint: disable=pointless-string-statement
+""" 
+#N Glider
+#O Richard K. Guy
+#C The smallest, most common, and first discovered spaceship. Diagonal, has period 4 and speed c/4.
+#C www.conwaylife.com/wiki/index.php?title=Glider
+x = 3, y = 3, rule = B3/S23
+bob$2bo$3o!
+"""
+METADATA: pp.ParserElement = (
+    (
+        pp.AtLineStart("#").suppress()
+        + pp.one_of("C c N O P R r")
+        + pp.OneOrMore(pp.Word(pp.alphas))
+    )
+    .set_results_name("metadata")
+    .set_name("metadata")
+)
 
 
 class RunLengthEncoded(FLContextManager):
@@ -14,12 +35,16 @@ class RunLengthEncoded(FLContextManager):
         self._file: TextIOWrapper
 
     def cells(self) -> list[list[bool]]:
-        """Return an array of cells loaded from the file."""
+        """Return an array of cells lopaded from the file."""
         return [[False]]
 
     def __enter__(self) -> FileLoader:
-        """Enter context manager."""
+        """Enter context manager which causes the file to be parsed immediately."""
         self._file = open(self._filename, "r", encoding="UTF-8")
+        # print(METADATA)
+        # parser:
+        # parsed_file = parser.parse_file(self._file)
+        # print(parsed_file)
         return self
 
     def __exit__(
--- python3/tests/test_file_loaders.py
+++ python3/tests/test_file_loaders.py
@@ -2,6 +2,17 @@
 
 # from pytest import raises
 from gameoflife import create_loader, FileLoader, RunLengthEncoded
+from gameoflife import runlengthencoded
+import pyparsing as pp
+
+GLIDER: str = """\
+#N Glider
+#O Richard K. Guy
+#C The smallest, most common, and first discovered spaceship. Diagonal, has period 4 and speed c/4.
+#C www.conwaylife.com/wiki/index.php?title=Glider
+x = 3, y = 3, rule = B3/S23
+bob$2bo$3o!
+"""
 
 
 def test_create_loader() -> None:
@@ -16,3 +27,14 @@ def test_run_length_encoded() -> None:
     """Test the RunLengthEncoded file type."""
     with create_loader("../data/glider.rle") as loader:
         assert loader.cells()[0][0] is False
+
+
+class TestRunLengthEncoded:
+    """Tests specifically for the class RunLengthEncoded."""
+
+    def test_metadata_parser(self) -> None:
+        """Test the METADATA parser."""
+        results: pp.ParseResults = runlengthencoded.METADATA.parse_string(GLIDER)
+        assert len(results) == 2  # N Glider
+        assert results[0] == "N"
+        assert results[1] == "Glider"
