97a82e2b6cd9e9fbd70ada55270bfd5d9a6423e6
Mats Henrikson <mats@zakalwe.com>
2023-06-13 14:41:09 +1000

    Parse and test actual RLE cell row data.

--- python3/gameoflife/runlengthencoded.py
+++ python3/gameoflife/runlengthencoded.py
@@ -20,34 +20,57 @@ bob$2bo$3o!
 class RunLengthEncoded(FLContextManager):
     """Implements loading Run Length Encoded data from files."""
 
-    METADATA_LINE: pp.ParserElement = (
+    _INT_NUMBER: pp.ParserElement = pp.Word(pp.nums).set_parse_action(  # type:ignore
+        lambda t: [int(t[0])]  # type:ignore
+    )
+
+    _CELL_STATES: pp.ParserElement = pp.one_of(" ".join(c for c in pp.alphas))
+
+    # N Glider
+    # O Richard K. Guy
+    # C www.conwaylife.com/wiki/index.php?title=Glider
+    _METADATA_LINE: pp.ParserElement = (
         pp.AtLineStart("#").suppress()
         + pp.one_of("C c N O P R r")
         + pp.Word(pp.printables + " ")
         + pp.Suppress(pp.line_end)
     ).set_results_name("metadata", True)
 
-    int_number: pp.ParserElement = pp.Word(pp.nums).set_parse_action(  # type:ignore
-        lambda t: [int(t[0])]  # type:ignore
-    )
-
-    HEADER: pp.ParserElement = pp.OneOrMore(
+    # x = 3, y = 3, rule = B3/S23
+    # x = 3, y = 3
+    _HEADER: pp.ParserElement = pp.OneOrMore(
         (
             pp.one_of("x y")
             + pp.Suppress("=")
-            + int_number
+            + _INT_NUMBER
             + pp.Optional(",").suppress()
         ).set_results_name("header", True)
     )
 
-    RULE: pp.ParserElement = pp.ZeroOrMore(
+    # x = 3, y = 3, rule = B3/S23
+    # x = 3, y = 3
+    _RULE: pp.ParserElement = pp.ZeroOrMore(
         pp.Keyword("rule")
         + pp.Suppress("=")
         + pp.Word(pp.printables + " ")
         + pp.Suppress(pp.line_end)
     ).set_results_name("rule")
 
-    PARSER: pp.ParserElement = pp.ZeroOrMore(METADATA_LINE) + HEADER + RULE
+    # bob$2bo$3o!
+    #
+    # 24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4b
+    # obo$10bo5bo7bo$11bo3bo$12b2o!
+    _CELL_ROWS: pp.ParserElement = (
+        pp.OneOrMore(
+            (
+                pp.OneOrMore(pp.Optional(_INT_NUMBER) + _CELL_STATES)
+                + pp.Optional(pp.Literal("$").suppress())
+            ).set_results_name("cell_rows", True)
+        )
+        + pp.Literal("!").suppress()
+    )
+
+    _PARSER: pp.ParserElement = pp.ZeroOrMore(_METADATA_LINE) + _HEADER + _RULE
 
     def __init__(self, file: str) -> None:
         """Initialise the loader."""
@@ -65,7 +88,7 @@ class RunLengthEncoded(FLContextManager):
     def __enter__(self) -> FileLoader:
         """Enter context manager which causes the file to be parsed immediately."""
         self._file = open(self._filename, "r", encoding="UTF-8")
-        results: pp.ParseResults = RunLengthEncoded.PARSER.parse_file(self._file)
+        results: pp.ParseResults = RunLengthEncoded._PARSER.parse_file(self._file)
         self.metadata = results.metadata  # type: ignore
         self.cols = results.header[0][1]  # type: ignore
         self.rows = results.header[1][1]  # type: ignore
--- python3/tests/test_file_loaders.py
+++ python3/tests/test_file_loaders.py
@@ -17,6 +17,12 @@ bob$2bo$3o!
 HEADER_RULE: str = "x = 4, y = 5, rule = B3/S23"
 HEADER_NO_RULE: str = "x = 4, y = 5"
 
+GLIDER_DATA: str = "bob$2bo$3o!"
+GOSPER_DATA: str = """\
+24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4b
+obo$10bo5bo7bo$11bo3bo$12b2o!
+"""
+
 
 def test_create_loader() -> None:
     """Test the create_loader function."""
@@ -46,32 +52,58 @@ def test_run_length_encoded() -> None:
         )
 
 
+# pylint: disable=protected-access
+# pyright: reportPrivateUsage=false
 class TestRunLengthEncoded:
     """Tests specifically for the class RunLengthEncoded."""
 
     def test_metadata_parser(self) -> None:
         """Test the METADATA parser."""
         results: pp.ParseResults = pp.ZeroOrMore(
-            RunLengthEncoded.METADATA_LINE
+            RunLengthEncoded._METADATA_LINE
         ).parse_string(GLIDER)
         assert len(results) == 8
         assert results[0] == "N"
         assert results[1] == "Glider"
         assert results[-1] == "www.conwaylife.com/wiki/index.php?title=Glider"
-        assert results.metadata[1][1] == "Richard K. Guy"  # type: ignore
+        assert len(results.metadata) == 4  # type:ignore
+        assert results.metadata[1][1] == "Richard K. Guy"  # type:ignore
 
     def test_header_rule_parser(self) -> None:
         """Test the HEADER parser."""
         results: pp.ParseResults = (
-            RunLengthEncoded.HEADER + RunLengthEncoded.RULE
+            RunLengthEncoded._HEADER + RunLengthEncoded._RULE
         ).parse_string(HEADER_RULE)
         assert results.header[0][1] == 4  # type:ignore
         assert results.header[1][1] == 5  # type:ignore
         assert results.rule[1] == "B3/S23"  # type:ignore
 
-        results = (RunLengthEncoded.HEADER + RunLengthEncoded.RULE).parse_string(
+        results = (RunLengthEncoded._HEADER + RunLengthEncoded._RULE).parse_string(
             HEADER_NO_RULE
         )
         assert results.header[0][1] == 4  # type:ignore
         assert results.header[1][1] == 5  # type:ignore
         assert len(results.rule) == 0  # type:ignore
+
+    def test_cell_rows_parser(self) -> None:
+        """Test the CELL_ROWS parser."""
+        results: pp.ParseResults = RunLengthEncoded._CELL_ROWS.parse_string(GLIDER_DATA)
+        assert len(results.cell_rows) == 3  # type:ignore
+        assert results.cell_rows[0].as_list() == ["b", "o", "b"]  # type:ignore
+        assert results.cell_rows[2].as_list() == [3, "o"]  # type:ignore
+
+        results = RunLengthEncoded._CELL_ROWS.parse_string(GOSPER_DATA)
+        assert len(results.cell_rows) == 9  # type:ignore
+        assert results.cell_rows[0].as_list() == [24, "b", "o"]  # type:ignore
+        assert results.cell_rows[6].as_list() == [  # type:ignore
+            10,
+            "b",
+            "o",
+            5,
+            "b",
+            "o",
+            7,
+            "b",
+            "o",
+        ]
+        assert results.cell_rows[8].as_list() == [12, "b", 2, "o"]  # type:ignore
