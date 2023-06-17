ba5b9c1e70266795a9f86ed91237a32ff8e8cb9a
Mats Henrikson <mats@zakalwe.com>
2023-06-17 15:19:33 +1000

    Parsers probably work.

--- python3/gameoflife/dataloaders/plaintext.py
+++ python3/gameoflife/dataloaders/plaintext.py
@@ -21,8 +21,33 @@ OOO
 class PlainText(FLContextManager):
     """Implements loading Plain Text data from files."""
 
-    # TODO: placeholder parser
-    _PARSER: pp.ParserElement = pp.Literal(" ")
+    # !Name: Glider
+    # !Author: Richard K. Guy
+    # !The smallest, most common, and first discovered spaceship.
+    # !www.conwaylife.com/wiki/index.php?title=Glider
+    _METADATA_LINE: pp.ParserElement = pp.ZeroOrMore(
+        (
+            pp.AtLineStart("!").suppress()
+            + (pp.Optional(pp.Word(pp.printables + " ")) + pp.line_end.suppress())
+            .set_whitespace_chars(" \t")
+            .set_results_name("metadata", True)
+        )
+    )
+
+    # is there an existing pyparsing element that has all alphas separated by spaces?
+    _CELL_STATES: pp.ParserElement = pp.one_of(". O").set_whitespace_chars(" \t")
+
+    # bob$2bo$3o!
+    #
+    # 24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4b
+    # obo$10bo5bo7bo$11bo3bo$12b2o!
+    _DATA_ROWS: pp.ParserElement = pp.OneOrMore(
+        (pp.OneOrMore(_CELL_STATES) + pp.line_end.suppress()).set_results_name(
+            "data_rows", True
+        )
+    )
+
+    _PARSER: pp.ParserElement = _METADATA_LINE + _DATA_ROWS
 
     def __init__(self, file: str) -> None:
         """Initialise the loader."""
@@ -34,7 +59,10 @@ class PlainText(FLContextManager):
         self._file = open(self._filename, "r", encoding="UTF-8")
         results: pp.ParseResults = PlainText._PARSER.parse_file(self._file)
         self.metadata = results.metadata.as_list()  # type:ignore
-        raise NotImplementedError()
+        print(self.metadata)  # type:ignore
+        print(results.data_rows.as_list())  # type:ignore
+        self.cells = []
+        return self
 
     def __exit__(
         self,
