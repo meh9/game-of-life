5837521c089030d072066c88a371228a1476aa2a
Mats Henrikson <mats@zakalwe.com>
2023-06-18 20:44:43 +1000

    Increase the number of times we run progress().

--- python3/tests/test_performance.py
+++ python3/tests/test_performance.py
@@ -10,19 +10,21 @@ LOGGER: logging.Logger = logging.getLogger(__name__)
 
 
 def test_dict_progress_large_file() -> None:
-    """Performance test goes here."""
+    """Performance test for the GameOfLifeDict().progress() method."""
     gol: GameOfLife = GameOfLifeDict()
+    LOGGER.info("'create_loader(period59glidergun.rle)' starting...")
     start: int = perf_counter_ns()
     with create_loader("../data/period59glidergun.rle") as loader:
         gol.add_cells(loader)
     last_gen_time: int = perf_counter_ns() - start
     LOGGER.info(
-        "'create_loader('period59glidergun.rle')' time: %s µs",
+        "'create_loader(...)' time: %s µs",
         round(last_gen_time / 1000),
     )
 
     # measure the progress() method
-    times: int = 10
+    times: int = 100
+    LOGGER.info("'gol.progress()' * %s starting...", times)
     start = perf_counter_ns()
     for _ in range(times):
         gol.progress()
