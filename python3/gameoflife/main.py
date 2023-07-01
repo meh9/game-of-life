d9ee96c549602acdde7901c3b4eb6e29649db34f
Mats Henrikson <mats@zakalwe.com>
2023-07-01 17:59:01 +1000

    Move save game WIP into its own function.

--- python3/gameoflife/main.py
+++ python3/gameoflife/main.py
@@ -170,24 +170,7 @@ class MainGame:
                 case "q":
                     self._run = False
                 case "s":
-                    self._automatic = False  # if we are running, stop
-                    # move down to the bottom of the screen
-                    with self._t.location(0, self._t.height - 1):
-                        print("Save RLE to path/filename: ", flush=True, end="")
-                        filename: str = ""
-                        stop: bool = False
-                        while not stop:
-                            save_key: Keystroke = self._t.inkey()
-                            if save_key.is_sequence:
-                                if int(self._t.KEY_ENTER) == save_key.code:
-                                    stop = True
-                            else:
-                                filename += save_key
-                                print(save_key, flush=True, end="")
-                    # TODO: save file happens here
-                    with self._t.location(0, self._t.height - 1):
-                        timestamp: str = datetime.now().strftime("%H:%M:%S")
-                        print(f"Saved game to file '{filename}' at {timestamp}", end="")
+                    self._save_game()
                 case "+" | "=" | "]":  # also accept "=" so we don't have use shift all the time
                     self._increase_speed()
                 case "-" | "[":
@@ -195,6 +178,27 @@ class MainGame:
                 case _:
                     pass  # do nothing with unrecognised keys
         return False
+
+    def _save_game(self) -> None:
+        """Save the game to a file."""
+        self._automatic = False  # if we are running, stop
+        # move down to the bottom of the screen
+        with self._t.location(0, self._t.height - 1):
+            print("Save RLE to path/filename: ", flush=True, end="")
+            filename: str = ""
+            stop: bool = False
+            while not stop:
+                save_key: Keystroke = self._t.inkey()
+                if save_key.is_sequence:
+                    if int(self._t.KEY_ENTER) == save_key.code:
+                        stop = True
+                else:
+                    filename += save_key
+                    print(save_key, flush=True, end="")
+        # TODO: save file happens here
+        with self._t.location(0, self._t.height - 1):
+            timestamp: str = datetime.now().strftime("%H:%M:%S")
+            print(f"Saved game to file '{filename}' at {timestamp}", end="")
 
     def _move_left(self) -> None:  # pragma: no cover
         """Move the view or edit cursor left."""
